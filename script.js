a142212326d5077ea3e4160ad7c8d4ba37383c2d
ralph27 <maaloulyralph@gmail.com>
2020-09-09 17:25:09 +0300

    making the header mobile friendly

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let navigations = document.querySelector(".navigations");
 let burger = document.querySelector(".burger");
+let body = document.querySelector(".body");
 
 function toggleNav() {
-  navigations.classList.toggle('active');
+  navigations.classList.toggle("active");
+  body.classList.toggle('background')
+  burger.classList.toggle("toggle");
 }
 
-
- burger.addEventListener("click", toggleNav);
+burger.addEventListener("click", toggleNav);
